517a9d9e7d44596d392a3d132d832479230904b7
Wren Steitle <lauren.steitle@gmail.com>
2019-10-23 09:16:19 -0600

    Complete initialization tests for Patron Class

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -3,7 +3,18 @@ require 'minitest/pride'
 require './lib/patron'
 
 class PatronTest < MiniTest::Test
+  def setup
+    @bob = Patron.new("Bob", 20)
+  end
 
+  def test_it_exists
+    assert_instance_of Patron, @bob
+  end
 
+  def test_initialize_data
+    assert_equal "Bob", @bob.name
+    assert_equal 20, @bob.spending_money
+    assert_equal [], @bob.interests
+  end
 
 end
