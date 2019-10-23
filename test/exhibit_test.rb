67420a70fcaee6bcc7b069245afceafd846951ad
Wren Steitle <lauren.steitle@gmail.com>
2019-10-23 09:12:10 -0600

    Create patron class and test file

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -1,15 +1,22 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/exhibit'
+require './lib/patron'
 
 class ExhibitTest < MiniTest::Test
 
   def setup
     @exhibit = Exhibit.new("Gems and Minerals", 0)
+    @bob = Patron.new("Bob", 20)
   end
 
   def test_it_exists
     assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_initialize_data
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
   end
 
 end
---
+++ test/patron_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+
+class PatronTest < MiniTest::Test
+
+
+
+end
