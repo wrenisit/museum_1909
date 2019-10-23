05ac67b13d544458458d85337cba9565e049a30b
Wren Steitle <lauren.steitle@gmail.com>
2019-10-23 09:20:38 -0600

    Complete patron add_interest method and tests

--- lib/patron.rb
+++ lib/patron.rb
@@ -6,4 +6,7 @@ class Patron
     @interests = []
   end
 
+  def add_interest(interest)
+    @interests << interest
+  end
 end
--- test/patron_test.rb
+++ test/patron_test.rb
@@ -17,4 +17,13 @@ class PatronTest < MiniTest::Test
     assert_equal [], @bob.interests
   end
 
+  def test_add_interest
+    assert_equal 0, @bob.interests.count
+    @bob.add_interest("Dead Sea Scrolls")
+    assert_equal 1, @bob.interests.count
+    @bob.add_interest("Gems and Minerals")
+    assert_equal 2, @bob.interests.count
+    assert_equal "Dead Sea Scrolls", @bob.interests.first
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @bob.interests
+  end
 end
